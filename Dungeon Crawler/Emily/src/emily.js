2476ab7cdcdbac895ba91c01c4a8bfd8658a75b4
emilyjiao <45907366+emilyjiao@users.noreply.github.com>
2021-02-06 18:55:52 -0500

    added put backpack function

---
+++ Dungeon Crawler/Emily/src/emily.js
@@ -0,0 +1,8 @@
+const game = new Phaser.Game(800, 600, Phaser.AUTO)
+
+//game.state.add("MenuScreen", Dungeon_Crawler.Menu)
+//game.state.add("SF_part_1", LEVEL1.SF_part_1)
+
+// game.state.add("End", End.Credits)
+// game.state.add("GameOver", Game_over.go_screen)
+// game.state.start("MenuScreen")
