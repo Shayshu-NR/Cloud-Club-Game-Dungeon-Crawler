979a8d9965e5fe14cf5dc4fec27a65fda406d5fa
Shayshu-NR <shayshu.nahata@mail.utoronto.ca>
2021-02-27 18:50:50 -0500

    Added no overlap logic to backpack

--- Dungeon Crawler/Emily/src/emily.js
+++ Dungeon Crawler/Emily/src/emily.js
@@ -7,6 +7,8 @@ const game = new Phaser.Game(
     }
 )
 
+var inventory
+
 function preload(){
     game.load.image('boots', '../Assets/General assets/Skill Tree/speed.png')
     game.load.image('arrow', '../Assets/General assets/Skill Tree/atks.png')
@@ -14,6 +16,15 @@ function preload(){
 }
 
 function create(){
+
+    inventory = []
+
+    for(var i = 0; i < 4; i++){
+        inventory.push([])
+        for(var j = 0; j < 4; j++){
+            inventory[i][j] = 0
+        }
+    }
 
     this.add.image(50,50,'backpack');
 
@@ -23,6 +34,13 @@ function create(){
 
     var arrow = item.create(70, 70, 'arrow')
     var shoe = item.create(140, 70, 'boots')
+
+    arrow.inv = [0, 0]
+    shoe.inv = [1, 0]
+
+    inventory[0][0] = 1
+    inventory[0][1] = 1
+
     shoe.inputEnabled = true;
     shoe.input.enableDrag();
     shoe.events.onDragStart.add(onDragStart, this);
@@ -53,7 +71,24 @@ function create(){
     }
     
     function onDragStop(sprite, pointer) {
+        var inv_x = (sprite.x / 70) - 1
+        var inv_y = (sprite.y / 70) - 1
         console.log(sprite.x+ ", "+sprite.y);
+
+        if(inventory[inv_y][inv_x] === 1){
+            sprite.position.x = (sprite.inv[0] + 1)*70
+            sprite.position.y = (sprite.inv[1] + 1)*70
+            return
+        }
+        else{
+            inventory[sprite.inv[1]][sprite.inv[0]] = 0
+            inventory[inv_y][inv_x] = 1
+
+            sprite.inv[0] = inv_x
+            sprite.inv[1] = inv_y
+        }
+        
+
     }
 }
 function update(){
