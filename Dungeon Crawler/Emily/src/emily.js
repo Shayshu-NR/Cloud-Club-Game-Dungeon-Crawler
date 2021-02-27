fe0aecc2402846d31d650cdc161bd07a00063a5d
emilyjiao <45907366+emilyjiao@users.noreply.github.com>
2021-02-26 23:46:43 -0500

    added item snap to backpack and created for loop to initialize backpack interface with items

--- Dungeon Crawler/Emily/src/emily.js
+++ Dungeon Crawler/Emily/src/emily.js
@@ -21,8 +21,8 @@ function create(){
 
     item.inputEnableChildren = true;
 
-    var arrow = item.create(50, 50, 'arrow')
-    var shoe = item.create(100,100, 'boots')
+    var arrow = item.create(70, 70, 'arrow')
+    var shoe = item.create(140, 70, 'boots')
     shoe.inputEnabled = true;
     shoe.input.enableDrag();
     shoe.events.onDragStart.add(onDragStart, this);
@@ -33,15 +33,27 @@ function create(){
     arrow.events.onDragStart.add(onDragStart, this);
     arrow.events.onDragStop.add(onDragStop, this);
 
-    // for  ( i; i < player.backpack; i++){
-
-    // }
+    //initializing backpack interface with items
+    // bpList = Object.keys(player.backpack)
+    // count = 0;
+    // for  (var i = 1; i <= 4; i++){
+    //     for (var j = 1; j <= 4; j++){
+    //         player.backpack[bpList[count]]["group"] = item.create(i*70,j*70,bpList[count])
+    //         player.backpack[bpList[count]]["group"].inputEnabled = true;
+    //         player.backpack[bpList[count]]["group"].input.enableDrag();
+    //         player.backpack[bpList[count]]["group"].events.onDragStart.add(onDragStart, this);
+    //         player.backpack[bpList[count]]["group"].events.onDragStop.add(onDragStop, this);
+    //         count++;
+    //     }
+    
     function onDragStart(sprite, pointer) {
         console.log("Dragging " + sprite.key);
+        sprite.input.enableSnap(70, 70, false, true);
+        
     }
     
     function onDragStop(sprite, pointer) {
-        console.log(sprite.key + " dropped at x:" + pointer.x + " y: " + pointer.y);
+        console.log(sprite.x+ ", "+sprite.y);
     }
 }
 function update(){
