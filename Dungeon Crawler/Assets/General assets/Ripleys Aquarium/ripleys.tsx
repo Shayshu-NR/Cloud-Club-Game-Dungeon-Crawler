ae02c78e5c2c18b54f200db9415a9907eec180bb
Bryce Lepasana <lepasanabryce@gmail.com>
2021-03-06 16:08:24 -0500

    finish level

--- Dungeon Crawler/Assets/General assets/Ripleys Aquarium/ripleys.tsx
+++ Dungeon Crawler/Assets/General assets/Ripleys Aquarium/ripleys.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="ripleys" tilewidth="32" tileheight="32" tilecount="30" columns="6">
- <image source="../../../../../../Documents/GitHub/Cloud-Club-Game-/Dungeon Crawler/Assets/General assets/Ripleys Aquarium/tileset.png" width="192" height="160"/>
+ <image source="tileset.png" width="192" height="160"/>
 </tileset>
