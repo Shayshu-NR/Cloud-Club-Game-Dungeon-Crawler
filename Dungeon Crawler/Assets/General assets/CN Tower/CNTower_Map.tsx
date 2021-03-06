6eb0a140f67f107455309f6aa9f2d4f9b5f37903
iLIrene <71670677+iLIrene@users.noreply.github.com>
2021-03-06 14:12:38 -0500

    Delete CNTower_Map.tsx

--- Dungeon Crawler/Assets/General assets/CN Tower/CNTower_Map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="CNTower_StructureTileset" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="CNTower_StructureTileset.png" width="160" height="160"/>
-</tileset>
