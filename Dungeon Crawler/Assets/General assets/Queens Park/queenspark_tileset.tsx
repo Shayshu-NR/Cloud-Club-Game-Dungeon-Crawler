65c959ab0e46ae9131871d115ea08b9e21379b26
Lepabryce <Lepasanabryce@gmail.com>
2022-01-03 17:08:30 -0500

    queens park level

---
+++ Dungeon Crawler/Assets/General assets/Queens Park/queenspark_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="queenspark_tileset" tilewidth="16" tileheight="16" tilecount="56" columns="7">
+ <image source="tileset.png" width="112" height="128"/>
+</tileset>
